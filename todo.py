dac1a6e479c7d092914e5a320914437c5912b597
Muhamed_Adebiyi <muhammedadebiyi2006@gmail.com>
2023-09-22 16:59:29 +0100

    second commit

---
+++ todo.py
@@ -0,0 +1,19 @@
+from tkinter import *
+from tkinter import ttk
+
+
+class Todo:
+    def __int__(self, root):
+        pass
+
+
+def main():
+    root = Tk()
+    ui = Todo(root)
+    root.mainloop()
+if __name__ == "___main___":
+    main()
+
+
+
+root.mainloop()
